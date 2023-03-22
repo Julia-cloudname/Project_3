e5d9e536609a768b352fa44610083f26c1ae8f00
Julia <malukjulia2014@gmail.com>
2023-03-22 23:28:45 +0000

    Create function to get user's data - height and weight

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_data():
+    """
+    Get data input from the user 
+    """
+    print("Please enter your height in centimeters")
+    print("Example: 162")
+
+    height = input("Enter your data here: ")
+
+    print("Please enter your weight in kilograms")
+    print("Example: 60")
+
+    weight = input("Enter your data here: ")
+    print(f'Your height is {height} cm and weight is {weight} kg')
+
+
+get_user_data()
