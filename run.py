fada24585e76c018431c3c1de017dd882e421189
Julia <malukjulia2014@gmail.com>
2023-03-28 22:00:42 +0000

    Add Input validation

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def get_user_height():
         except ValueError:
             print("\nNot a number. Please, enter your height as a number\n")
 
-        
+
 def get_user_weight():
     """
     Get data input from the user - weight
@@ -30,7 +30,7 @@ def get_user_weight():
                 return user_weight
         except ValueError:
             print("\nNot a number. Please, enter your weight as a number\n")
-        
+
 
 def get_user_age():
     """
@@ -55,13 +55,16 @@ def get_user_gender():
     Get data input from the user - gender
     """
     while True:
-        print("\nPlease enter 'female' if you are woman and 'male' if you are man")
+        print("\nPlease enter 'female' if you are woman"
+              "and 'male' if you are man")
         print("Example: female\n")
         try:
             user_gender = input("Enter your data here:\n")
 
-            if user_gender != 'female' or user_gender != 'male':
-                print(f"\nYou entered {user_gender}. Please, enter 'male' or 'female'\n")
+            if user_gender != 'female' and user_gender != 'male':
+                print(
+                    f"\nYou entered {user_gender}."
+                    f"Please, enter 'male' or 'female'\n")
             else:
                 return user_gender
         except ValueError:
@@ -72,16 +75,23 @@ def get_user_activity():
     """
     Get data input from the user - activity
     """
-    print("\nPlease enter data about your activity")
-    print("""\n    Little or no exercise: press 1\n
-    1–3 days per week of exercise or activity: press 2\n
-    3–5 days per week of moderate activity or sports: press 3\n
-    6–7 days per week of hard exercise: press 4\n
-    athletes who train twice per day or more: press 5""")
-    print("Example: 2\n")
-    user_activity = input("Enter your data here:\n")
-
-    return user_activity
+    while True:
+        print("\nPlease enter data about your activity")
+        print("""\n    Little or no exercise: press 1\n
+        1 - 3 days per week of exercise or activity: press 2\n
+        3 - 5 days per week of moderate activity or sports: press 3\n
+        6 - 7 days per week of hard exercise: press 4\n
+        athletes who train twice per day or more: press 5""")
+        print("Example: 2\n")
+        try:
+            user_activity = input("Enter your data here:\n")
+            if user_activity not in ['1', '2', '3', '4', '5']:
+                print("\nPlease enter a valid number of activity")
+            else:
+                return int(user_activity)
+        except ValueError:
+            print("""\nNot a number. Please, enter your user
+            activity as a number\n""")
 
 
 def get_user_goal():
@@ -98,97 +108,101 @@ def get_user_goal():
     return user_goal
 
 
-def basal_metabolic_rate_woman(weight, height, age):
+def bmr_woman(weight_value, height_value, age_value):
     """
     Calculate basal metabolic rate for women
     """
-    bmr = 655.1 + (9.6 * weight) + (1.85 * height) - (age * 4.68) 
+    bmr_value = 655.1 + \
+        (9.6 * weight_value) + \
+        (1.85 * height_value) - \
+        (age_value * 4.68)
     # source - Wikipedia
-    print(f"\nWeight: {weight}, height: {height}, age: {age}, gender: woman")
-    print(f"\nYour basal metabolic rate is {int(bmr)} calories per day.")
-    print("""\n    Basal metabolic rate (BMR) - minimum amount of calories 
-    that our body needs to carry out basic functions such
-    as breathing, circulation, and cell production while 
-    at rest. In other words, it is the  amount of 
-    energy required by our body to keep functioning 
-    while we are doing nothing.""")
-    return bmr
+    print(f"\nWeight: {weight_value}, height: {height_value},"
+          f" age: {age_value}, gender: woman")
+    print(f"\nYour basal metabolic rate is {int(bmr_value)} calories per day.")
+    print("\n\tBasal metabolic rate (BMR) - minimum amount of calories"
+          "that our body needs to carry out basic functions such"
+          "as breathing, circulation, and cell production while "
+          "at rest. In other words, it is the  amount of"
+          "energy required by our body to keep functioning"
+          "while we are doing nothing.")
+    return bmr_value
 
 
-def basal_metabolic_rate_man(weight, height, age):
+def bmr_man(weight_value, height_value, age_value):
     """
     Calculate basal metabolic rate for men
     """
-    bmr = 66.5 + (13.7 * weight) + (5 * height) - (age * 6.75)
+    bmr_value = 66.5 + \
+        (13.7 * weight_value) + \
+        (5 * height_value) - \
+        (age_value * 6.75)
     # source - Wikipedia
-    print(f"Weight:{weight}, height:{height}, age:{age}, gender: man")
-    print(f"Your basal metabolic rate is {int(bmr)} calories per day.")
-    print("""\n    Basal metabolic rate (BMR) - minimum amount of calories 
-    that our body needs to carry out basic functions such
-    as breathing, circulation, and cell production while 
-    at rest. In other words, it is the  amount of 
-    energy required by our body to keep functioning 
-    while we are doing nothing.\n""")
-    return bmr
+    print(f"Weight:{weight_value}, height:{height_value}"
+          f"age:{age_value}, gender: man")
+    print(f"Your basal metabolic rate is {int(bmr_value)} calories per day.")
+    print("\nBasal metabolic rate (BMR) - minimum amount of calories"
+          " that our body needs to carry out basic functions such"
+          " as breathing, circulation, and cell production while"
+          " at rest. In other words, it is the  amount of"
+          " energy required by our body to keep functioning"
+          " while we are doing nothing.\n")
+    return bmr_value
 
 
-def calculate_bmr(weight, height, age, gender):
+def calculate_bmr(weight_value, height_value, age_value, gender_value):
     """
     Calculate BMR depend on gender
     """
-    if (gender == "male"):
-        return basal_metabolic_rate_man(weight, height, age)
+    if (gender_value == "male"):
+        return bmr_man(weight_value, height_value, age_value)
     else:
-        return basal_metabolic_rate_woman(weight, height, age)
+        return bmr_woman(weight_value, height_value, age_value)
 
 
-def calorie_calculator(activity, bmr):
-    """ 
-    Calculate calorie needs based on BMR
-    """
-    if activity == "1":
-        calorie_needs = bmr * 1.2
-    elif activity == "2":
-        calorie_needs = bmr * 1.37
-    elif activity == "3":
-        calorie_needs = bmr * 1.55
-    elif activity == "4":
-        calorie_needs = bmr * 1.72
+def calorie_calculator(activity_value, bmr_value):
+    "Calculate calorie needs based on BMR"
+    if activity_value == "1":
+        calorie_needs = bmr_value * 1.2
+    elif activity_value == "2":
+        calorie_needs = bmr_value * 1.37
+    elif activity_value == "3":
+        calorie_needs = bmr_value * 1.55
+    elif activity_value == "4":
+        calorie_needs = bmr_value * 1.72
     else:
-        calorie_needs = bmr * 1.9        
+        calorie_needs = bmr_value * 1.9
 
-    return int(calorie_needs) 
+    return int(calorie_needs)
 
 
-def goal_calorie():
-    """ 
-    Calculate calorie needs depend on user goal
-    """
-    get_height = get_user_height()
-    get_weight = get_user_weight()
+def goal_calorie(goal_value, daily_calorie_value):
+    "Calculate calorie needs depend on user goal"
 
-    get_age = get_user_age()
-    get_gender = get_user_gender()
-    get_activity = get_user_activity()
-    get_goal = get_user_goal()
-    
-    get_bmr = calculate_bmr(get_weight, get_height, get_age, get_gender)
-    daily_calorie = calorie_calculator(get_activity, get_bmr)
-    
-    if get_goal == "lose":
-        new_calorie = daily_calorie - 500
-    elif get_goal == "gain":
-        new_calorie = daily_calorie + 500
+    if goal_value == "lose":
+        return int(daily_calorie_value - 500)
+    elif goal_value == "gain":
+        return int(daily_calorie_value + 500)
     else:
-        new_calorie = daily_calorie
-
-    print(f"\nYour daily calorie needs are:\n    {daily_calorie} calories per day")
-    print(f"\nTo {get_goal} weight eat:")
-    
-    return int(new_calorie)
+        return int(daily_calorie_value)
 
 
 if __name__ == "__main__":
-    
-    get_goal_calorie = goal_calorie()
-    print(f"    {get_goal_calorie} calories per day")
+
+    height = get_user_height()
+    weight = get_user_weight()
+
+    age = get_user_age()
+    gender = get_user_gender()
+    activity = get_user_activity()
+    goal = get_user_goal()
+
+    bmr = calculate_bmr(weight, height, age, gender)
+    daily_calorie = calorie_calculator(activity, bmr)
+
+    goal_calorie = goal_calorie(goal, daily_calorie)
+
+    print("\nYour daily calorie needs are:")
+    print(f"\n{daily_calorie} calories per day")
+
+    print(f"\nTo {goal} weight eat: {goal_calorie}")
